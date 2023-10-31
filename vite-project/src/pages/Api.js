d09af236c89893000049a6667218ac4a85d8c660
poposito07 <139826515+poposito07@users.noreply.github.com>
2023-10-30 23:11:36 -0600

    Update Api.js

--- vite-project/src/pages/Api.js
+++ vite-project/src/pages/Api.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000; // Elige el puerto que desees
+const port = 3000;
 
 app.use(bodyParser.json());
 
-// Array para almacenar el historial de conversiones (simulado)
+
 const conversionHistory = [];
 
-// Ruta para obtener el historial de conversiones
+
 app.get('/get-history', (req, res) => {
   res.json(conversionHistory);
 });
 
-// Ruta para agregar una nueva conversión al historial
 app.post('/add-conversion', (req, res) => {
   const { fromCurrency, toCurrency, amount, result, date } = req.body;
 
